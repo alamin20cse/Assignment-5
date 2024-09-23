10a69f106c2ea5fd25d84a66036e66aaad9beab8
alamin20cse <md.alamin165086@gmail.com>
2024-09-23 15:37:23 +0600

    number convart

--- utilities.js
+++ utilities.js
@@ -5,7 +5,7 @@ function getInputFieldValueById(id)
 {
 
 
-    const InputValue=Number(document.getElementById(id).value).toFixed(2);
+    const InputValue=Number(document.getElementById(id).value);
     document.getElementById(id).value='';
     return InputValue;
     
@@ -18,7 +18,7 @@ function getInputFieldValueById(id)
 function getTextFieldValueById(id)
 {
     const textValue=document.getElementById(id).innerText;
-    const textNumber=Number(textValue).toFixed(2);
+    const textNumber=Number(textValue);
     return textNumber;
 
 }
